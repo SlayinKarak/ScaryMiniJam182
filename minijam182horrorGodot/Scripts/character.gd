6d66a8f4884faea7a2dd7e150c3db8c3434073a0
Crxyn <xincero211@gmail.com>
2025-04-13 16:04:12 -0400

    Sounds

--- minijam182horrorGodot/Scripts/character.gd
+++ minijam182horrorGodot/Scripts/character.gd
@@ -3,6 +3,34 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
+@onready var walk_player = $WalkPlayer
+
+var footstep_sounds = [
+	preload("res://Sounds/SFX/Grass1.mp3"),
+	preload("res://Sounds/SFX/Grass2.mp3"),
+	preload("res://Sounds/SFX/Grass3.mp3"),
+	preload("res://Sounds/SFX/Grass4.mp3")
+]
+
+var shuffle_bag = []
+
+func _ready():
+	reshuffle_bag()
+	
+func play_random_footstep():
+	if shuffle_bag.is_empty():
+		reshuffle_bag()
+
+	var sound = shuffle_bag.pop_back()
+	walk_player.stream = sound
+	walk_player.play()
+	
+func reshuffle_bag():
+	shuffle_bag = footstep_sounds.duplicate()
+	shuffle_bag.shuffle()
+#Sorry i did way to much just to make Footsteps not repeat over and over
+
+var Walk_timer = 0.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,6 +39,16 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction != 0 and is_on_floor():
+		Walk_timer -= delta
+		if Walk_timer <= 0:
+			play_random_footstep()
+			Walk_timer = 0.5
+	else:
+		
+		Walk_timer = 0.0
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
--- minijam182horrorGodot/Scripts/level_1.gd
+++ minijam182horrorGodot/Scripts/level_1.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 @onready var cam = $Camera2D
 @onready var player = $CharacterBody2D
+@onready var Ambience = $Ambience
+
+func _ready() -> void:
+	Ambience.play()
 
 func _process(delta: float) -> void:
 	cam.set_position(player.get_position())
+ 
